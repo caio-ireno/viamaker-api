855262b91e1969e1e8cbbca1bbcb160d6f5b5a06
caio-ireno <caio_ireno@hotmail.com>
2023-07-15 15:38:20 -0300

    Feat: Cors

--- dist/src/app.js
+++ dist/src/app.js
@@ -6,11 +6,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dbConnection_1 = __importDefault(require("./config/dbConnection"));
 const routes_1 = __importDefault(require("./routes"));
+const cors_1 = __importDefault(require("cors"));
 dbConnection_1.default.on("error", console.log.bind(console, 'Erro de conexão'));
 dbConnection_1.default.once("open", () => {
     console.log('conexão com o banco feita com sucesso');
 });
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
+app.use((0, cors_1.default)({
+    origin: 'http://localhost:5173'
+}));
 (0, routes_1.default)(app);
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import db from "./config/dbConnection";
 import routes from "./routes";
+import cors from 'cors';
+
 
 db.on("error", console.log.bind(console, 'Erro de conexão'))
 db.once("open", () => {
@@ -9,6 +11,9 @@ db.once("open", () => {
 
 const app = express();
 app.use(express.json())
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
 routes(app);
 
 export default app
